752b47c77fa6203ecc0fc424a401b00c06f66805
Valmir Barbosa <valmir.php@gmail.com>
2024-05-09 23:54:39 -0300

    feat: template

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -18,30 +18,30 @@ if (process.env.NODE_ENV !== 'test') {
 }
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.24',
   defaultNetwork: 'hardhat',
-  // TODO revisar
-  // etherscan: {
-  //   apiKey: {
-  //     arbitrumOne: process.env.ARBISCAN_API_KEY || "",
-  //     avalanche: process.env.SNOWTRACE_API_KEY || "",
-  //     bsc: process.env.BSCSCAN_API_KEY || "",
-  //     mainnet: process.env.ETHERSCAN_API_KEY || "",
-  //     optimisticEthereum: process.env.OPTIMISM_API_KEY || "",
-  //     polygon: process.env.POLYGONSCAN_API_KEY || "",
-  //     polygonMumbai: process.env.POLYGONSCAN_API_KEY || "",
-  //     sepolia: process.env.ETHERSCAN_API_KEY || "",
-  //   },
-  // },
-  // gasReporter: {
-  //   currency: "USD",
-  //   token: "ETH",
-  //   gasPriceApi: "Etherscan",
-  //   gasPrice: 21,
-  //   enabled: !!process.env.REPORT_GAS,
-  //   excludeContracts: [],
-  //   src: "./contracts",
-  // },
+  etherscan: {
+    apiKey: {
+      arbitrumOne: process.env.ARBISCAN_API_KEY || '',
+      avalanche: process.env.SNOWTRACE_API_KEY || '',
+      bsc: process.env.BSCSCAN_API_KEY || '',
+      mainnet: process.env.ETHERSCAN_API_KEY || '',
+      optimisticEthereum: process.env.OPTIMISM_API_KEY || '',
+      polygon: process.env.POLYGONSCAN_API_KEY || '',
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY || '',
+      sepolia: process.env.ETHERSCAN_API_KEY || '',
+    },
+  },
+  gasReporter: {
+    currency: 'USD',
+    token: 'ETH',
+    gasPriceApi: 'Etherscan',
+    gasPrice: 21,
+    enabled: !!process.env.REPORT_GAS,
+    excludeContracts: [],
+    src: './contracts',
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    showMethodSig: true,
+  },
   networks: {
     hardhat: {
       accounts: {
@@ -76,6 +76,15 @@ const config: HardhatUserConfig = {
   typechain: {
     outDir: './storage/typechain',
     target: 'ethers-v6',
+  },
+  solidity: {
+    version: '0.8.24',
+    settings: {
+      optimizer: {
+        enabled: process.env.SOLIDITY_OPTIMIZER_ENABLED === 'true',
+        runs: 200,
+      },
+    },
   },
 };
 
